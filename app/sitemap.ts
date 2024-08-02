027d4d7de84d8b1fe3e74d2caed7568a877427d6
Andrew🍕 <andrewalangm@gmail.com>
2024-08-02 17:30:50 -0600

    Update domain name in metadata files

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://andrew-dev.io',
+      url: 'https://andrewgm.me',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://andrew-dev.io/work',
+      url: 'https://andrewgm.me/work',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://andrew-dev.io/about',
+      url: 'https://andrewgm.me/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: 'https://andrew-dev.io/blog',
+      url: 'https://andrewgm.me/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.6,
